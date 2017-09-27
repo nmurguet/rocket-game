1c8904ed7e7925013d1d1f6daf2bb698eae3edee
Nicolas <nefisto@gmail.com>
2017-09-27 16:18:28 -0300

    Adds
    -Added morelevels
    -Added Missile Mechanic

---
+++ rocket/Assets/scripts/CameraFollow.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+	public Camera myCamera; 
+	public Transform target; 
+	public float speed; 
+	public bool following; 
+
+	public float offset_x; 
+	public float offset_y; 
+
+	public float zoom; 
+
+	public float minZoom; 
+	public float maxZoom; 
+
+	public float maxSpeed; 
+
+	private Rocket player; 
+
+	// Use this for initialization
+	void Start () {
+		target = GameObject.FindGameObjectWithTag ("Player").transform; 
+		myCamera = Camera.main; 
+		player = GameObject.FindObjectOfType<Rocket> (); 
+		following = true; 
+
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (following) {
+			Vector3 offset = new Vector3 (offset_x, offset_y, -10f);
+			if (target) {
+				transform.position = Vector3.Lerp (transform.position + offset, target.position, speed);
+
+			}
+		}
+
+		if (player.velocity > maxSpeed) {
+			myCamera.orthographicSize += 0.05f; 
+
+		} else if (player.velocity < maxSpeed) {
+			myCamera.orthographicSize -= 0.05f; 
+
+		}
+		if (myCamera.orthographicSize > maxZoom) {
+			myCamera.orthographicSize = maxZoom; 
+
+		}
+		if (myCamera.orthographicSize < minZoom) {
+			myCamera.orthographicSize = minZoom; 
+
+		}
+
+	}
+}
---
+++ rocket/Assets/scripts/HomingMissile.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HomingMissile : MonoBehaviour {
+
+	public float speed = 5f; 
+	public float rotatingSpeed = 200f; 
+	private GameObject target; 
+
+	public GameObject deathEffect; 
+	private Rigidbody2D rb; 
+	// Use this for initialization
+	void Start () {
+		target = GameObject.FindGameObjectWithTag ("Player"); 
+		rb = GetComponent<Rigidbody2D> (); 
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		
+	}
+
+
+	void FixedUpdate(){
+		Vector2 point2Target = (Vector2)transform.position - (Vector2)target.transform.position; 
+
+		point2Target.Normalize (); 
+
+		float value = Vector3.Cross (point2Target, transform.right).z;
+
+		rb.angularVelocity = rotatingSpeed * value; 
+		rb.velocity = transform.right * speed; 
+
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{ if (other.tag == "Player") {
+			other.GetComponent<Rigidbody2D> ().velocity = new Vector2 (rb.velocity.x * 10f, rb.velocity.y * 10f);
+			//other.GetComponent<Rigidbody2D> ().AddForce (Vector2.right * 800f);
+			Destroy (this.gameObject, 0.02f);
+		} else {
+		     Destroy (this.gameObject, 0.02f);
+			Instantiate (deathEffect, transform.position, Quaternion.identity);
+		}
+
+	}
+}
