712f8e603f6c60628be6871e849f4e0269ec19c3
Nicolas <nefisto@gmail.com>
2017-09-28 17:15:37 -0300

    Adds
    -Added more levels
    -Added Missiles and homing Cannon

---
+++ rocket/Assets/scripts/CannonLauncher.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonLauncher : MonoBehaviour {
+
+	public GameObject bullet; 
+	public GameObject spawnBullet; 
+
+	public GameObject activeBullet; 
+
+	// Use this for initialization
+	void Start () {
+		Instantiate (bullet, spawnBullet.transform.position, Quaternion.identity);
+		activeBullet = GameObject.FindGameObjectWithTag ("homingMissile");
+		
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (activeBullet.GetComponent<HomingMissile> ().isDestroyed == true) {
+			StartCoroutine( WaitandShoot (2));
+					
+			}
+
+
+
+
+	}
+
+
+	IEnumerator WaitandShoot(int seconds)
+	{
+		yield return new WaitForSeconds (seconds);
+		activeBullet.GetComponent<HomingMissile> ().Shoot (spawnBullet.transform);
+
+	}
+}
--- rocket/Assets/scripts/HomingMissile.cs
+++ rocket/Assets/scripts/HomingMissile.cs
@@ -10,10 +10,25 @@ public class HomingMissile : MonoBehaviour {
 
 	public GameObject deathEffect; 
 	private Rigidbody2D rb; 
+	public bool isDestroyed; 
+
+	//variables for storing
+	private float storeSpeed;
+	private SpriteRenderer sr; 
+	private bool isTargeting; 
+	private TrailRenderer tr; 
+	private CapsuleCollider2D cc;
+
 	// Use this for initialization
 	void Start () {
 		target = GameObject.FindGameObjectWithTag ("Player"); 
 		rb = GetComponent<Rigidbody2D> (); 
+		tr = GetComponent<TrailRenderer> ();
+		cc = GetComponent<CapsuleCollider2D> (); 
+		isDestroyed = false; 
+		storeSpeed = speed; 
+		sr = GetComponent<SpriteRenderer> ();
+		isTargeting = true; 
 		
 	}
 	
@@ -26,26 +41,60 @@ public class HomingMissile : MonoBehaviour {
 
 
 	void FixedUpdate(){
-		Vector2 point2Target = (Vector2)transform.position - (Vector2)target.transform.position; 
+		if (isTargeting) {
+			Vector2 point2Target = (Vector2)transform.position - (Vector2)target.transform.position; 
 
-		point2Target.Normalize (); 
+			point2Target.Normalize (); 
 
-		float value = Vector3.Cross (point2Target, transform.right).z;
+			float value = Vector3.Cross (point2Target, transform.right).z;
 
-		rb.angularVelocity = rotatingSpeed * value; 
-		rb.velocity = transform.right * speed; 
+			rb.angularVelocity = rotatingSpeed * value; 
+			rb.velocity = transform.right * speed; 
+		}
 
 	}
 
 	void OnTriggerEnter2D(Collider2D other)
 	{ if (other.tag == "Player") {
 			other.GetComponent<Rigidbody2D> ().velocity = new Vector2 (rb.velocity.x * 10f, rb.velocity.y * 10f);
-			//other.GetComponent<Rigidbody2D> ().AddForce (Vector2.right * 800f);
-			Destroy (this.gameObject, 0.02f);
-		} else {
-		     Destroy (this.gameObject, 0.02f);
+			rb.velocity = new Vector2 (0, 0);
+			tr.enabled = false; 
+			sr.enabled = false;
+			isTargeting = false;
+			cc.enabled = false; 
+
 			Instantiate (deathEffect, transform.position, Quaternion.identity);
+			//Destroy (this.gameObject, 0.02f);
+			isDestroyed = true; 
+
+		} else {
+		     //Destroy (this.gameObject, 0.02f);
+			rb.velocity = new Vector2 (0, 0);
+			isDestroyed = true; 
+			isTargeting = false;
+			
+			tr.enabled = false; 
+			sr.enabled = false;
+			cc.enabled = false; 
+			
+			
+			Instantiate (deathEffect, transform.position, Quaternion.identity);
+			
 		}
+
+	}
+
+	public void Shoot(Transform spawn)
+	{
+		
+		cc.enabled = true; 
+		isTargeting = true; 
+		transform.position = new Vector2 (spawn.position.x, spawn.position.y); 
+		sr.enabled = true; 
+		speed = storeSpeed; 
+		isDestroyed = false; 
+		tr.enabled = true; 
+
 
 	}
 }
